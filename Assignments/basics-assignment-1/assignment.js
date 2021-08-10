ff7e52b2ff951c89d0b0704a689026c69eff7796
vkay11 <vinaykhanduri2001@gmail.com>
2021-08-10 13:12:58 +0530

    Completed Assignment 1

---
+++ Assignments/basics-assignment-1/assignment.js
@@ -0,0 +1,22 @@
+// Task 1
+let userInputVariable = 5;
+let result;
+
+
+// Task 2
+result = 18 + userInputVariable;
+
+
+// Task 3
+result = result - 5;
+result = result * 10;
+result = result / 5;
+
+
+// Task 4
+// Answer - The value stored in userInputVariable wouldn't change!
+
+
+// Task 5
+alert(result);
+alert(userInputVariable);
