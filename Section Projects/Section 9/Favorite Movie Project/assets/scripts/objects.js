c463ad0498a95a2b7e8e8ff7752e6b805c994cd2
vkay11 <vinaykhanduri2001@gmail.com>
2021-08-21 23:56:36 +0530

    Added Search Functionality

--- Section Projects/Section 9/Favorite Movie Project/assets/scripts/objects.js
+++ Section Projects/Section 9/Favorite Movie Project/assets/scripts/objects.js
@@ -4,6 +4,41 @@ const searchButton = document.getElementById("search-btn");
 let movies = [];
 let idCounter = 0;
 
+function renderMovies(filter = '') {
+    const movieList = document.getElementById('movie-list');
+    
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    }
+    else {
+        movieList.classList.add('visible');
+    }
+    
+    movieList.innerHTML = '';
+
+    let filteredMovies = [];
+
+    if (!filter) {
+        filteredMovies = movies;
+    }
+    else {
+        filteredMovies = movies.filter(movie => movie.info.title.includes(filter));
+    }
+
+    filteredMovies.forEach(movie => {
+        const movieElement =    document.createElement('li');
+        let text = movie.info.title + ' - ';
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text + `${key}: ${movie.info[key]}`;
+            }
+        }
+        movieElement.textContent = text;
+        movieList.append(movieElement); 
+    })
+}
+
 function addMovieHandler() {
     const title = document.getElementById("title").value;
     const extraName = document.getElementById("extra-name").value;
@@ -27,7 +62,15 @@ function addMovieHandler() {
 
     movies.push(newMovie);
     idCounter++;
-    console.log(newMovie);
+    renderMovies();
 }
 
+
+function searchMovieHandler() {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+}
+
+
 addMovieButton.addEventListener('click', addMovieHandler);
+searchButton.addEventListener('click', searchMovieHandler);
